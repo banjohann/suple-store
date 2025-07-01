ba0500f672c25597120213a17bfbf29c2fbe4e40
Johann Bandelow <johannabandelow@gmail.com>
2025-07-01 18:38:06 -0300

    Feito mapeamento direto para Customer X deliveryAddress

--- loja-suplementos/src/main/java/com/loja/suplementos/customer/domain/Customer.java
+++ loja-suplementos/src/main/java/com/loja/suplementos/customer/domain/Customer.java
@@ -1,9 +1,13 @@
 package com.loja.suplementos.customer.domain;
 
+import com.loja.suplementos.address.DeliveryAddress;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,6 +16,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -37,6 +43,10 @@ public class Customer {
     private String cpf;
 
     private Date birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "customerId")
+    private Set<DeliveryAddress> deliveryAddresses = new HashSet<>();
 
     public String getFullName() {
         return name + " " + lastName;
